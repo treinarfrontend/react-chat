970c9e1986beff44fe7b2200505a196d0aea3fe9
Vitor Leonel <falecom@vitorleonel.com>
2018-02-18 02:36:15 -0300

    navbar base

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,8 +4,17 @@ import './Navbar.css'
 const Navbar = props => (
     <nav className="navbar">
 
-        <div className="profile">
+        {/*<div className="navbar__item menu">*/}
+            {/*<a href="" className="menu__item">Menu 1</a>*/}
+            {/*<a href="" className="menu__item">Menu 2</a>*/}
+        {/*</div>*/}
 
+        <div className="navbar__item profile">
+            <div className="profile__name">Avatar Name</div>
+
+            <figure className="profile__avatar">
+                <img src="http://i.pravatar.cc/300" alt="Avatar Name"/>
+            </figure>
         </div>
 
     </nav>
