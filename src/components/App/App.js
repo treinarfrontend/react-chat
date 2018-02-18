d5e146c780485be08ca8e51c3854fc369dd42a68
Vitor Leonel <falecom@vitorleonel.com>
2018-02-17 22:46:47 -0200

    change import method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import classes from './App.css'
+import './App.css'
 
 import Contacts from '../Contacts/Contacts'
 
@@ -7,7 +7,7 @@ class App extends Component {
 
     render () {
         return (
-            <div className={classes.wrapper}>
+            <div className="wrapper">
                 <Contacts />
             </div>
         )
