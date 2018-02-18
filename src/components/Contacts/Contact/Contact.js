a741e9177357729679b2f610a5e30e04e1f2446c
Vitor Leonel <falecom@vitorleonel.com>
2018-02-18 01:39:31 -0300

    change contacts container

--- src/components/Contacts/Contact/Contact.js
+++ src/components/Contacts/Contact/Contact.js
@@ -3,13 +3,13 @@ import './Contact.css'
 
 const Contact = props => (
     <div className="contact">
-        <div className="avatar">
-            <img src="http://i.pravatar.cc/300" alt="Avatar"/>
-        </div>
+        <figure className="contact__avatar">
+            <img src="http://i.pravatar.cc/300" alt="Avatar Name"/>
+        </figure>
 
-        <div className="details">
-            <h2 className="name">Avatar Name</h2>
-            <span className="last-message">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</span>
+        <div className="contact__details">
+            <h2 className="contact__details--name">Avatar Name</h2>
+            <p className="contact__details--message">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
         </div>
     </div>
 )
